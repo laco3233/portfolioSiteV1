4716897d68b568759b0b4339f2114c18ff146ae7
laco3233 <lacombetrevor@gmail.com>
2024-03-18 12:19:39 -0700

    Added insult generator

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ let firstName = document.querySelector(".firstName");
 
 
 //insult generator
+let insult = {
+    adjective1: ["bald", "horrible", "terrible", "silly", "dumb", "smelly"],
+    adjective2: ["gawdy", "foolish", "ugly", "high calorie", "grotesque"],
+    noun: ["clown", "sponge", "fool", "Mount Rushmore", "shell of a human"],
+}
+
+let insultGenerator = () => {
+    let adjective1 = insult.adjective1[Math.floor(Math.random() * insult.adjective1.length)];
+    let adjective2 = insult.adjective2[Math.floor(Math.random() * insult.adjective2.length)];
+    let noun = insult.noun[Math.floor(Math.random() * insult.noun.length)];
+
+    console.log(`You absolute ${adjective1} ${adjective2} ${noun}`);
+}
